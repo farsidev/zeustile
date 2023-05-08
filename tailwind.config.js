4516fe1f2f4028667fb3bec1017a0f35cb28d6fc
Ali <aliwsome@gmail.com>
2023-05-08 11:54:20 +0330

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#FFDF00",
+      },
+    },
+    fontFamily: {
+      iransans: ['IranSans', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
